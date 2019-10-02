d9f52684ced4c51f99218f46cd222dd8721356e6
Audun Halland <audun.halland@gmail.com>
2019-10-02 21:19:47 +0200

    Add env vars

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 
+const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID || '';
+const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET || '';
+const SLACK_SIGNING = process.env.SLACK_SIGNING_SECRET || '';
+
 express()
   .get('/email-addresses', (req, res) => {
-    res.send('my email!');
+    res.send(`my bot with id ${SLACK_CLIENT_ID}`);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
