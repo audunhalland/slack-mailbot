34e7ff89f079ede4a74aa86d9c7279a9de0c43c9
Audun Halland <audun.halland@gmail.com>
2019-10-02 21:11:11 +0200

    Send something

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .get('/email-addresses', (req, res) => {
-    res.send('');
+    res.send('my email!');
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
